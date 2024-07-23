4b3cafd18b9cb59f7ced274f6fc7cf890bba8f8f
bkhebert <bondman2004tippy@gmail.com>
2024-07-23 16:30:51 -0500

    search err

--- functions.js
+++ functions.js
@@ -21,7 +21,7 @@ C:
 E:
 */
 
-function search(animalsArr, animalName){
+function search(animalsArr, animalName) {
     // create a for loop that goes through the animals Array
         for(var i = 0; i < animalsArr.length; i++){
             // loop through the animalsArr to see if any names match
